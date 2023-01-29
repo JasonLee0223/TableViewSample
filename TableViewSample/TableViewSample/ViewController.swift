55d7e4a10a200c6858eb3089a412599cb1711cdb
Jason <ggj00125@gmail.com>
2023-01-30 01:25:38 +0900

    Feat: 활동학습 내용까지 구현

--- TableViewSample/TableViewSample/ViewController.swift
+++ TableViewSample/TableViewSample/ViewController.swift
@@ -14,20 +14,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = dataSource
+        self.configureTableView()
     }
 
-    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self.dataSource
+        
+        tableView.rowHeight = 100
+    }
 }
 
 extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.showAlert(indexPath: indexPath)
+        // 사용자와의 상호작용했을때를 보기 위해서 delegate method이다.
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
+    private func showAlert(indexPath: IndexPath) {
+        let alertTitle = "어머나"
+        let alertMessage = "날 만졌어\n \(indexPath.section), \(indexPath.row)"
+        let actionTitle = "그랬구나"
+        let alert = UIAlertController(title: alertTitle,
+                                      message: alertMessage,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: actionTitle, style: .default)
+        alert.addAction(action)
+        
+        present(alert, animated: true)
+    }
 }
 
 class sampleTableViewDataSource: NSObject, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 10
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -35,7 +56,17 @@ class sampleTableViewDataSource: NSObject, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cellIdentifier = "tableVIewCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        //MARK: - https://zeddios.tistory.com/1206 1205번도 봐야한다! textLabel 14이상에서 지원X
+        //cell.textLabel?.text = "\(indexPath.section), \(indexPath.row)"
+        var cellContent = cell.defaultContentConfiguration()
+        cellContent.text = "\(indexPath.section), \(indexPath.row)"
+        cell.contentConfiguration = cellContent
+        
+        
+        return cell
     }
     
     
