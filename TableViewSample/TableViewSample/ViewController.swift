299065e6b84eba206d1290aa66d6d163b449af05
Jason <ggj00125@gmail.com>
2023-01-30 00:49:17 +0900

    Feat: 프로젝트 초기설정
    - TableView 및 TableViewCell 스토리보드상 추가 및 연결

--- TableViewSample/TableViewSample/ViewController.swift
+++ TableViewSample/TableViewSample/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+class sampleTableViewDataSource: NSObject, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
